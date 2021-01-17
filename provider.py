3b747be4eff34636f4a5ce5056e76982e87d7bea
sam <shnachman@gmail.com>
2021-01-17 12:38:04 -0500

    moved alert notification

--- provider.py
+++ provider.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+import random
 
 import requests
 
@@ -26,8 +27,10 @@ class Provider():
             file.close()
 
     def get_data(self):
-        ua = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
-        headers = {"User-Agent": ua}
+        uas = ["Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36", "Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36", "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1"]
+        current_ua = random.choice(uas)
+        print(current_ua)
+        headers = {"User-Agent": current_ua}
         r = requests.get(self.url, headers=headers)
         return r
 
@@ -43,9 +46,9 @@ class Provider():
             if self.check_for_claimed_phrase(data.text):
                 results = "No appts"
                 self.log(results)
-                self.alert(results + "\n" + data.url)
             else:
                 results = "APPTS AVAILABLE"
+                self.alert(results + "\n" + data.url)
                 self.log(results)
         else:
             self.log(str(data.status_code))

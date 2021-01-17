6ce4383c9f423370713b70f4146c7d82eee67c93
sam <shnachman@gmail.com>
2021-01-17 13:46:24 -0500

    added additional site to scrape

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from provider import Provider
 
 # https://www.marylandvax.org/clinic/search?service%5B%5D=Vaccination&location=Prince+George%27s+County%2C+MD%2C+USA&search_radius=100%2B+miles&search_name=&search_date=&commit=Search#search_results
+# https://www.gbmc.org/covid-vaccine
 
 if __name__ == '__main__':
     stagnes = Provider("St. Agnes", "Due to overwhelming demand Ascension Saint Agnes has no vaccination appointments available at this time", "https://healthcare.ascension.org/COVID-19/vaccinations")

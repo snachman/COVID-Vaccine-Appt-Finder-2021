86f45a5dc073815fa55d5b62c67f84197c7a751c
sam <shnachman@gmail.com>
2021-01-18 09:15:51 -0500

    disabled umms

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ from provider import Provider
 if __name__ == '__main__':
     stagnes = Provider("St. Agnes", "Due to overwhelming demand Ascension Saint Agnes has no vaccination appointments available at this time", "https://healthcare.ascension.org/COVID-19/vaccinations")
     stagnes.act()
-    umms = Provider("UMMS", "Please check back here Monday, January 18 for more details", "https://www.umms.org/coronavirus/covid-vaccine/get-vaccine")
-    umms.act()
+    # umms = Provider("UMMS", "Please check back here Monday, January 18 for more details", "https://www.umms.org/coronavirus/covid-vaccine/get-vaccine")
+    # umms.act()
     # test

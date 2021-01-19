5927b2111d8d3645ab034ae9701a637d8ec28207
sam <shnachman@gmail.com>
2021-01-19 04:29:36 -0500

    removed comment

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ import utils
 from provider import Provider
 
 # https://www.marylandvax.org/clinic/search?service%5B%5D=Vaccination&location=Prince+George%27s+County%2C+MD%2C+USA&search_radius=100%2B+miles&search_name=&search_date=&commit=Search#search_results
-# https://www.gbmc.org/covid-vaccine
 
 if __name__ == '__main__':
     utils.log("Begin vax search")
     stagnes = Provider("St. Agnes", "Due to overwhelming demand Ascension Saint Agnes has no vaccination appointments available at this time", "https://healthcare.ascension.org/COVID-19/vaccinations")
-    stagnes.act()
+    # stagnes.act()
     umms = Provider("UMMS", "Please check back here Monday, January 18 for more details", "https://www.umms.org/coronavirus/covid-vaccine/get-vaccine")
     # umms.act()
     gbmc = Provider("GBMC", "All available slots filled. Check back soon!", "https://www.gbmc.org/covid-vaccine")
-    gbmc.act()
+    # gbmc.act()
     garrett = Provider("Garrett Co.", "THERE ARE NO APPOINTMENTS AVAILABLE AT THIS TIME", "https://health.maryland.gov/allegany/Pages/COVIDVaccination.aspx")
-    garrett.act()
+    # garrett.act()
+    giant = Provider("Giant Foods", "The following Giant Food locations will provide vaccinations to those who are eligible. Continue to check this site. More information will be provided as it becomes available", "https://coronavirus.maryland.gov/pages/maryland-retail-vaccination-sites")
+    giant.act()
+
     time.sleep(3)
     utils.log("End vax search")

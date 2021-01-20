1c2b4f2a1ac72103c9758b2b4bc5ad8340209564
sam <shnachman@gmail.com>
2021-01-20 14:46:38 -0500

    moved obsolete to own method

--- main.py
+++ main.py
@@ -17,13 +17,16 @@ def known_working():
     utils.log("End vax search")
 
 
-
-
-if __name__ == '__main__':
+def obsolete():
     # umms = Provider("UMMS", "Please check back here Monday, January 18 for more details", "https://www.umms.org/coronavirus/covid-vaccine/get-vaccine")
     # garrett = Provider("Garrett Co.", "THERE ARE NO APPOINTMENTS AVAILABLE AT THIS TIME", "https://health.maryland.gov/allegany/Pages/COVIDVaccination.aspx")
     # giant = Provider("Giant Foods", "The following Giant Food locations will provide vaccinations to those who are eligible. Continue to check this site. More information will be provided as it becomes available", "https://coronavirus.maryland.gov/pages/maryland-retail-vaccination-sites")
     # calvert = Provider("Calvert Co.", "The pre-registration vaccine portal will open at 10 a.m. on Tuesday, Jan. 19, 2021", "https://www.calvertcountymd.gov/Vaccine")
+    pass
+
+
+
+if __name__ == '__main__':
     fred = Provider("Frederick Co.", "All Clinics currently full, check back on Tues. Jan. 19", "https://health.frederickcountymd.gov/629/COVID-19-Vaccine")
 
 

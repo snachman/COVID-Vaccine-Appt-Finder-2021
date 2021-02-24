a7c0f7a475e3bdce5a1d56e1c13a7e56459286a6
sam <shnachman@gmail.com>
2021-02-23 21:07:27 -0500

    removed defunct if statement

--- provider.py
+++ provider.py
@@ -19,9 +19,8 @@ class Provider():
         uas = ["Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36", "Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36", "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1"]
         current_ua = random.choice(uas)
         headers = {"User-Agent": current_ua}
-        if "cvs" not in self.organization.lower():
-            r = requests.get(self.url, headers=headers)
-            return r
+        r = requests.get(self.url, headers=headers)
+        return r
 
     def check_for_claimed_phrase(self, body):
         if self.test_phrase in body:

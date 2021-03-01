cfe931ca0581f714eb8e6e60d417c0bca6b11946
sam <shnachman@gmail.com>
2021-03-01 05:25:07 -0500

    init

---
+++ locations-monitor/monitor.py
@@ -0,0 +1,8 @@
+import requests
+import utils
+
+
+if __name__ == '__main__':
+    arcgis = "https://services.arcgis.com/njFNhDsUCentVYJW/arcgis/rest/services/MD_Vaccination_Locations/FeatureServer/4/query?where=1%3D1&outFields=*&outSR=4326&f=json"
+    r = requests.get(arcgis)
+    json = (r.text)

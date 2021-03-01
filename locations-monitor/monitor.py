12abb34a093b2fc42a58db65bf4374a829ed4b1c
sam <shnachman@gmail.com>
2021-03-01 06:31:48 -0500

    removed unused variable

--- locations-monitor/monitor.py
+++ locations-monitor/monitor.py
@@ -25,7 +25,6 @@ def hash_data(string):
 
 
 if __name__ == '__main__':
-    i = 0
     arcgis = "https://services.arcgis.com/njFNhDsUCentVYJW/arcgis/rest/services/MD_Vaccination_Locations/FeatureServer/4/query?where=1%3D1&outFields=*&outSR=4326&f=json"
     r = requests.get(arcgis)
     j = json.loads(r.text)

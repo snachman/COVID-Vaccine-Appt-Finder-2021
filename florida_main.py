37c43c4f6e7daa6fc14ec3727a8b2556a4533ce0
sam <shnachman@gmail.com>
2021-03-24 13:18:55 -0400

    init

--- florida_main.py
+++ florida_main.py
@@ -1,12 +1,13 @@
 import requests
 import os
+import utils
 
-def alert(message):
+def florida_alert(message):
     end = ""
     cmd = """curl -X POST -H 'Content-type: application/json' --data '{"text":" %s"}' https://hooks.slack.com/services/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""" % (message)
     os.popen(cmd)
 
-def florida_act():
+def publix_act():
     url = "https://www.publix.com/covid-vaccine/florida/florida-county-status.txt?t=1616597688230"
     r = requests.get(url)
     lines = (r.content.decode("utf-16").splitlines())
@@ -14,8 +15,20 @@ def florida_act():
         parsed = line.split("|")
         town = parsed[0]
         status = parsed[1]
+        formatted = f"PUBLIX\n{town}: {status}"
         if "dade" in line.lower():
-            alert(f"PUBLIX\n{town}: {status}")
+            if "Fully Booked" in status:
+                utils.log(formatted)
+            elif "None Available" in status:
+                utils.log(formatted)
+            elif "Coming Soon".lower() in "status".lower():
+                utils.log(formatted)
+            else:
+                florida_alert(formatted + "\nhttps://www.publix.com/covid-vaccine/florida")
+
+
+
+
 
 if __name__ == '__main__':
-    florida_act()
+    publix_act()
